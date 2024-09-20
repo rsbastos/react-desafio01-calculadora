1340fe3ef57f48d436cbdcf6db018b9eb11facf9
rsbastos <renedossantosbastos@gmail.com>
2024-09-20 11:09:26 -0300

    :sparkles: Feat - 0 / dot buttons added

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ const App = () => {
   const handleSumNumbers = () => {
     if(firstNumber === '0'){
       setFirstNumber(String(currentNumber));
-      setCurrentNumber('0')
+      setCurrentNumber('0')      
       setOperation('+')
-    } else {      
+    } else {  
       const sum = Number(firstNumber) + Number(currentNumber);
       setCurrentNumber(String(sum))
       setOperation('')
@@ -94,28 +94,35 @@ const App = () => {
         <Input value={currentNumber}/>
         <Row>
           <Button label="C" onClick={handleOnClear}/>
-          <Button label="CE" onClick={handleOnClear}/>
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="." onClick={() => handleAddNumber('.')}/>
           <Button label="/" onClick={handleDivisionNumbers}/>
-          <Button label="x" onClick={handleMultiplicationNumbers}/>
+          
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleSubtractionNumbers}/>
+          <Button label="x" onClick={handleMultiplicationNumbers}/>
+          
         </Row> 
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="-" onClick={handleSubtractionNumbers}/>
+          
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={handleEquals}/>
-        </Row>       
+          <Button label="+" onClick={handleSumNumbers}/>
+          
+        </Row>     
+        <Row>      
+          <Button label="=" onClick={handleEquals}/>          
+        </Row>  
       </Content>      
     </Container>
   );
